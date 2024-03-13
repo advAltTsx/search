066621f3e946bee97647bfdbfe1573709c233130
Vasooli Bhai <154480782+advAltTsx@users.noreply.github.com>
2024-03-13 21:16:27 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/searchEngine',
-        permanent: true,
-      },
-    ]
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
