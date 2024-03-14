4264a30f14529c484c157ba086dc749b6430519e
Vasooli Bhai <154480782+advAltTsx@users.noreply.github.com>
2024-03-14 11:33:28 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,35 +1,37 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Footer from "@/components/Footer";
-import ChangePath from "@/components/ChangePath";
-import { Suspense } from "react";
-import localFont from '@next/font/local'
-
+import { Inter } from 'next/font/google';
+import './globals.css';
+import Footer from '@/components/Footer';
+import ChangePath from '@/components/ChangePath';
+import { Suspense } from 'react';
+import localFont from '@next/font/local';
+import Head from 'next/head';
 
 // const inter = Inter({ subsets: ["latin"] });
 const nothing = localFont({
   src: [
     {
       path: './nothing.ttf',
-      weight: '800'
+      weight: '800',
     },
   ],
-  variable: '--font-nothing'
-})
+  variable: '--font-nothing',
+});
 
 export const metadata = {
-  title: "Modern Search",
+  title: 'Modern Search Engine',
   icons: {
-    icon: '/logo.jpg'
+    icon: '/logo.jpg',
   },
-  description: "Nothing Styled Search Engine. Indie Project.",
+  description: 'Nothing Styled Search Engine. Indie Project.',
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${nothing.variable} font-sans`}>
-<link rel="icon" href="/logo.jpg" />
       {/* 080808 */}
+      <Head>
+        <link rel="shortcut icon" href="/logo.jpg" />
+      </Head>
       <body className={`} bg-[#080808]`}>
         {/* <body className={`${inter.className} bg-[#080808]`}> */}
         <Suspense>
